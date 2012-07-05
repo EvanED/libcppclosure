301126c6cd918d9f3cd031125af85389333f5199
Evan Driscoll <driscoll@cs.wisc.edu>
2012-07-04 20:42:14 -0500

    One more demo: use lambda

--- test.cc
+++ test.cc
@@ -35,5 +35,14 @@ int main()
   std::cout << &(return_ref_test2.get_func_ptr()(1,1)) << "\n";
   std::cout << &g << "\n";
 
+  auto l = [](int x, int y) { return x*y; };
+  std::function<int (int, int)> times = l;
+  ffi_function::CCallableClosure<int (int, int)> times2 = times;
+  int (*c_ptr)(int, int);
+  c_ptr = times2.get_func_ptr();
+
+  std::cout << c_ptr(4,5) << "\n";
+
+
   return 0;
 }
